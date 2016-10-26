a2cf293fd444037cd08f79a2f93e6bcf6b36fb08
Justin Weisz <jweisz@us.ibm.com>
2016-10-26 13:17:07 -0700

    oops

--- CardKitRuntime/Execution/Executable Cards/ExecutableTokenCard.swift
+++ CardKitRuntime/Execution/Executable Cards/ExecutableTokenCard.swift
@@ -13,7 +13,7 @@ import CardKit
 open class ExecutableTokenCard: CarriesTokenCardState {
     var tokenCard: TokenCard
     
-    open init(with card: TokenCard) {
+    public init(with card: TokenCard) {
         self.tokenCard = card
     }
 }
